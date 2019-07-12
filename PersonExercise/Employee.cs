0ecf7d8a151c2d78a3544767dba4884f7ec924db
Marcin Pleban <pleban.marcin@gmail.com>
2019-07-12 13:38:43 +0200

    Exercise3 end

--- PersonExercise/Employee.cs
+++ PersonExercise/Employee.cs
@@ -4,30 +4,42 @@ using System.Text;
 
 namespace PersonExercise
 {
-    class Employee : Person
+    class Employee : Person, ICloneable
     {
         public decimal Salary { get; set; }
         public string Profession { get; set; }
 
         public Room room;
 
-        public Employee(string name, DateTime birthDate, decimal salary, string profession, Room roomNumber) : base(name, birthDate)
+        public Employee(string name, DateTime birthDate, decimal salary, string profession) : base(name, birthDate)
         {
             this.Salary = salary;
             this.Profession = profession;
-            this.room = roomNumber;
+         
         }
         public override string ToString()
         {
             
             return base.ToString() + $"\nWypłata to: {Salary} w zawodzie {Profession}. Mieszka w pokoju {room.RoomNumber}";
         }
-        
+
+        public object Clone2()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public object Clone()
+        {
+            Employee newEmployee = (Employee)this.MemberwiseClone();
+            newEmployee.room = new Room(room.RoomNumber);
+
+            return newEmployee;
+        }
     }
 
     class Room
     {
-        public int RoomNumber { get; }
+        public int RoomNumber { get; set; }
 
 
         public Room(int roomNumber)
@@ -39,8 +51,13 @@ namespace PersonExercise
     {
         static void Main(string[] args)
         {
-            Person per1 = new Employee("Marcin", new DateTime(1985, 01, 21), 1600.66M, "Hydraulik", new Room(56));
-            Console.WriteLine(per1);
+            Employee Kovacs = new Employee("Géza", DateTime.Now, 1000M, "léhűtő");
+            Kovacs.room = new Room(111);
+            Employee Kovacs2 = (Employee)Kovacs.Clone();
+            Kovacs2.room.RoomNumber = 112;
+            Console.WriteLine(Kovacs.ToString());
+            Console.WriteLine(Kovacs2.ToString());
+            Console.ReadKey();
         }
     }
 }
