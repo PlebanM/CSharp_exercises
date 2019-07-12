31dce5bb870a05d0403d5ab41cea47a1f0f940b1
Marcin Pleban <pleban.marcin@gmail.com>
2019-07-12 11:00:57 +0200

    New class Employee

---
+++ PersonExercise/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonExercise
+{
+    class Employee : Person
+    {
+        public decimal Salary { get; set; }
+        public string Profession { get; set; }
+
+        public Room room;
+
+        public Employee(string name, DateTime birthDate, decimal salary, string profession, Room roomNumber) : base(name, birthDate)
+        {
+            this.Salary = salary;
+            this.Profession = profession;
+            this.room = roomNumber;
+        }
+        public override string ToString()
+        {
+            
+            return base.ToString() + $"\nWypłata to: {Salary} w zawodzie {Profession}. Mieszka w pokoju {room.RoomNumber}";
+        }
+        
+    }
+
+    class Room
+    {
+        public int RoomNumber { get; }
+
+
+        public Room(int roomNumber)
+        {
+            this.RoomNumber = roomNumber;
+        }
+    }
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            Person per1 = new Employee("Marcin", new DateTime(1985, 01, 21), 1600.66M, "Hydraulik", new Room(56));
+            Console.WriteLine(per1);
+        }
+    }
+}
+
--- PersonExercise/Program.cs
+++ PersonExercise/Program.cs
@@ -2,11 +2,35 @@
 
 namespace PersonExercise
 {
-    class Program
+    class Person
     {
-        static void Main(string[] args)
+
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Person(string name, DateTime birthDate)
         {
-            Console.WriteLine("Hello World!");
+            this.Name = name;
+            this.BirthDate = birthDate;
         }
+        public enum Genders { male, female }
+
+        private string WriteGender()
+        {
+            if (Name.EndsWith("a"))
+            {
+                return $"{Genders.female}";
+            }
+            else
+            {
+                return $"{Genders.male}";
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Name} urodzony: {BirthDate.Year} jako {WriteGender()}";
+        }
+
     }
+    
 }
